7bb3b20d5d45f0695f6bb2f738937b426950fa40
Sumeet Haldar <sumeet@Sumeets-MacBook-Air.local>
2024-09-21 02:51:40 +0530

    add new apis - getStory, getStoryByUserId, updateStory, getBookmarkAndLike and updateLikeCounts

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,104 +1,190 @@
 const expressAsyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const Story = require("../models/storyModal");
 const User = require("../models/userModal");
 
-
 const addStory = expressAsyncHandler(async (req, res) => {
-    const sessionUserId = req.userId;
-    const { slides } = req?.body;
-    
-    const payload = {
-        userId: sessionUserId,
-        slides: slides
-    }
+  const sessionUserId = req.userId;
+  const { slides } = req?.body;
+  
+  if(!slides || slides.length === 0) {
+    res.status(400)
+    throw new Error("slides parameter is required and connot be empty")
+  }
+  const payload = {
+    userId: sessionUserId,
+    slides: slides,
+  };
 
-    const newStory = new Story(payload);
-    const saveStory = await newStory.save();
-  
-    if (!saveStory) {
-      res.status(400);
-      throw new Error("Unable to add story");
-    }
-  
-    res.status(201).json({
-      message: "story added successfully",
-      data: saveStory,
-      success: true,
-      error: false,
-    });
+  const newStory = new Story(payload);
+  const saveStory = await newStory.save();
+
+  if (!saveStory) {
+    res.status(400);
+    throw new Error("Unable to add story");
+  }
+
+  res.status(201).json({
+    message: "story added successfully",
+    data: saveStory,
+    success: true,
+    error: false,
   });
+});
 
 
-  const userAction = expressAsyncHandler(async (req, res) => {
-    const { storyId, newBookmark, newLike } = req.body;
-    const userId = req.userId;
+const getStory = expressAsyncHandler(async (req, res) => {
+  const { storyId } = req.body;
 
-    if (!storyId) {
-      res.status(400);
-      throw new Error("storyId is required");
-    }
+  if(!storyId) {
+    res.status(404)
+    throw new Error("StoryId not found")
+  }
 
-    // if(newBookmark.length === 0 && newLike.length === 0) {
-    //   res.status(400);
-    //   throw new Error("both bookmark or like cannot be empty");
-    // }
-  
-    const user = await User.findById(userId);
-    const userActionIndex = user.userAction.findIndex(
-      (action) => action.storyId.toString() === storyId
-    );
+  const story = await Story.findById(storyId);
 
-    console.log("index: ", userActionIndex);
-    if (userActionIndex > -1) {
-      const currentAction = user.userAction[userActionIndex];
-      currentAction.bookmark = newBookmark;
-      currentAction.like = newLike;
-
-      if(newBookmark.length === 0 && newLike.length === 0) {
-        user.userAction.splice(userActionIndex, 1);
-      }
-      
-      // if (currentAction.bookmark) {
-      //   if (currentAction.like) {
-      //     user.userAction[userActionIndex].bookmark = false;
-      //   } else {
-      //     user.userAction.splice(userActionIndex, 1);
-      //   }
-      // }else {
-      //    user.userAction[userActionIndex].bookmark = true;
-      // }
-    } else {
-      if(newBookmark.length !== 0 || newLike.length !== 0) {
-        console.log("inside else")
-        const newAction = {
-          storyId: storyId,
-          bookmark: newBookmark,
-          like: newLike
-        };
-        user.userAction.push(newAction);
-      }
-    }
-  
-    const updatedUser = await user.save();
-  
-    res.status(200).json({
-      success: true,
-      error: false,
-      data: updatedUser.userAction
-    });
+  res.status(200).json({
+    message: "story fetched successfully",
+    success: true,
+    error: false,
+    data: story,
   });
+});
 
 
- 
-  module.exports = { addStory, userAction };  
+const getStoriesByUserId = expressAsyncHandler(async (req, res) => {
+  const userId = req.userId;
+  const stories = await Story.find({ userId });
+  
+  res.status(200).json({
+    message: "story fetched successfully",
+    success: true,
+    error: false,
+    data: stories,
+  });
+});
 
 
+const updateStory = expressAsyncHandler(async (req, res) => {
+  const { storyId, slides } = req.body;
 
-  // add story
-  // get story 
-  // update story 
-  // bookmark click
-  // like per click
-  // ?story=123456&slide=1
+  if(!storyId) {
+    res.status(404)
+    throw new Error("StoryId not found")
+  }
+  
+  const story = await Story.findById(storyId);
+  story.slides = slides;
+  const saveStory = await story.save();
+
+  res.status(200).json({
+    message: "story updated successfully",
+    success: true,
+    error: false,
+    data: saveStory,
+  });
+});
+
+
+const userAction = expressAsyncHandler(async (req, res) => {
+  const { storyId, newBookmark, newLike } = req.body;
+  const userId = req.userId;
+
+  if (!storyId || !newBookmark || !newLike) {
+    res.status(400);
+    throw new Error("storyId, bookmark and Like is required");
+  }
+
+  const user = await User.findById(userId);
+  const userActionIndex = user.userAction.findIndex(
+    (action) => action.storyId.toString() === storyId
+  );
+
+  if (userActionIndex > -1) {
+    const currentAction = user.userAction[userActionIndex];
+    currentAction.bookmark = newBookmark;
+    currentAction.like = newLike;
+
+    if (newBookmark.length === 0 && newLike.length === 0) {
+      user.userAction.splice(userActionIndex, 1);
+    }
+  } else {
+    if (newBookmark.length !== 0 || newLike.length !== 0) {
+      const newAction = {
+        storyId: storyId,
+        bookmark: newBookmark,
+        like: newLike,
+      };
+      user.userAction.push(newAction);
+    }
+  }
+  const updatedUser = await user.save();
+
+  res.status(200).json({
+    message: "bookmark and like updated successfully",
+    success: true,
+    error: false,
+    data: updatedUser.userAction,
+  });
+});
+
+
+const updateLikeCounts = expressAsyncHandler(async (req, res) => {
+  const {storyId, slideId, like} = req.body;
+  const story = await Story.findById(storyId);
+  if(!story) {
+    res.status(400)
+    throw new Error("story doesn't exist");
+  }
+  if(slideId >= story.slides.length) {
+    res.status(404);
+    throw new Error("Slide not found");
+  }
+  
+  story.slides[slideId].likes += like;
+  story.markModified('slides');
+  const updatedStory = await story.save();
+
+  res.status(200).json({
+    message: "like updated successfully",
+    success: true,
+    error: false,
+    data: updatedStory
+  });
+});
+
+
+const getBookmarkAndLike = expressAsyncHandler(async (req, res) => {
+  const userId = req.userId;
+  const user = await User.findById(userId);
+
+  if(!user) {
+    res.status(400);
+    throw new Error("user doesn't exist");
+  }
+
+  res.status(200).json({
+    message: "bookmark and like fetched successfully",
+    success: true,
+    error: false,
+    data: user.userAction
+  })
+});
+
+module.exports = { 
+  addStory, 
+  getStory, 
+  getStoriesByUserId,
+  updateStory, 
+  userAction, 
+  getBookmarkAndLike, 
+  updateLikeCounts 
+};
+
+// add story -- done
+// get story -- done
+// update story -- done
+// get story by userId -- done
+// bookmark like update -- done
+// get bookmark and like -- done
+// like count per click -- done
+// ?story=123456&slide=1
--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const validateToken = require("../middleware/validateTokenHandler");
-const { addStory, userAction } = require("../controllers/storyController");
+const { addStory, userAction, getBookmarkAndLike, updateLikeCounts, getStory, updateStory, getStoriesByUserId } = require("../controllers/storyController");
 
 const router = express.Router();
 
 router.post("/add", validateToken, addStory);
+router.get("/get", validateToken, getStory);
+router.get("/get-userid", validateToken, getStoriesByUserId);
+router.post("/update", validateToken, updateStory);
 router.post("/action", validateToken, userAction);
-
+router.get("/bookmark-like", validateToken, getBookmarkAndLike);
+router.post("/update-like", validateToken, updateLikeCounts);
 
 module.exports = router;
